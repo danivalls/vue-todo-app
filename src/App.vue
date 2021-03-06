ba4e09343b52fb76b1b1d32746d5a4fd3e704eae
Daniel Valls <daniel.valls.mestre@gmail.com>
2021-03-06 19:17:51 +0100

    Fixed overflow issue in tasks list

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
-    <div class="card task-input-card">
+    <div class="card">
       <add-task-input />
     </div>
     <div class="card tasks-list-card">
-      <tasks-list />
-      <clear-completed-tasks-button />
+      <tasks-list class="tasks-list" />
+      <clear-completed-tasks-button class="clear-list-button" />
     </div>
   </div>
 </template>
@@ -32,11 +32,10 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: $secondary;
-  height: 100vh;
   display: flex;
   flex-flow: column;
-  justify-content: center;
   align-items: center;
+  padding: 30px;
   row-gap: 15px;
 
   .card {
@@ -44,6 +43,16 @@ export default {
     padding: 15px;
     border-radius: 7px;
     box-shadow: 0 0 20px 10px $gray;
+    &.tasks-list-card {
+      .tasks-list {
+        max-height: 600px;
+        overflow-y: auto;
+        overflow-x: hidden;
+      }
+      .clear-list-button {
+        margin-top: 15px;
+      }
+    }
   }
 }
 </style>
